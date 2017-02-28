dbdb2e55593c77ab107a666ddeba26168dec0666
xhsdf <xhsdf.misc@googlemail.com>
2017-02-28 15:27:09 +0100

    clean-up

--- pxdo.py
+++ pxdo.py
@@ -5,18 +5,18 @@ NAME = "pxdo"
 VERSION = "0.10"
 
 
-import sys, re
-import Xlib.display
-import Xlib.X as X
-import Xlib.Xatom as Xatom
-import Xlib.protocol as protocol
-import Xlib.ext.randr as randr
+from sys import exc_info, argv
+from re import match, compile
+from Xlib.display import Display
+from Xlib.ext.randr import get_screen_resources, get_output_info
+from Xlib.protocol.event import ClientMessage
+from Xlib.X import AnyPropertyType, SubstructureRedirectMask, SubstructureNotifyMask
 
 display = None
 MOVE_REGEX = '--move-(0x[0-9a-fA-F]+)-(\\d+)x(\\d+)\\+(\\d+)\\+(\\d+)'
 
 def main(argv):
-	globals()['display'] = Xlib.display.Display()
+	globals()['display'] = Display()
 
 	for arg in argv:
 		try:
@@ -30,13 +30,13 @@ def main(argv):
 				print_window_info()
 			elif arg == '--get-monitor-info':
 				print_monitor_info()
-			elif re.match(MOVE_REGEX, arg):
-				p = re.compile(MOVE_REGEX)
+			elif match(MOVE_REGEX, arg):
+				p = compile(MOVE_REGEX)
 				wid, width, height, x, y = p.findall(arg)[0]
 				w = Window.from_id(int(wid, 16), False)
 				w.move(int(x), int(y), int(width), int(height))
 		except:
-			print("Unexpected error:", sys.exc_info()[0])
+			print("Unexpected error:", exc_info()[0])
 
 	display.sync()
 	display.flush()
@@ -66,14 +66,14 @@ class Monitor:
 		self.name = ""
 
 
-	# <name>	<id>	<geometry>
+	# <name> <id> <geometry>
 	def get_info_string(self):
 		return "%s\t%d\t%dx%d+%d+%d" % (self.name, self.id, self.width, self.height, self.x, self.y)
 
 
 	@staticmethod
 	def get_monitors():
-		resources = randr.get_screen_resources(display.screen().root)
+		resources = get_screen_resources(display.screen().root)
 		monitors = {}
 		for crtc in resources.crtcs:
 			crtc_info = display.xrandr_get_crtc_info(crtc, resources.config_timestamp)
@@ -81,21 +81,17 @@ class Monitor:
 				monitor = Monitor(output_id, crtc_info.x, crtc_info.y, crtc_info.width, crtc_info.height)
 				monitors[output_id] = monitor
 
-
-		output_ids = randr.get_screen_resources(display.screen().root).outputs
+		output_ids = resources.outputs
 		for monitor in monitors.values():
-			output_info = randr.get_output_info(display.screen().root, monitor.id, 0)
+			output_info = get_output_info(display.screen().root, monitor.id, 0)
 			monitor.name = output_info.name
-
-		#~ for monitor in monitors.values():
-			#~ print("MONITOR: " + monitor.get_info_string())
 		return monitors.values()
 
 
 class Window:
 	def __init__(self, win, win_id, full = True):
 		self.win = win
-		self.win_id = win_id
+		self.id = win_id
 		self.active = False
 		self.name = self.wm_class = "Unknown"
 		self.workspace = -1
@@ -128,9 +124,9 @@ class Window:
 	def get_windows():
 		windows = []
 		active_window_id = Window.get_active_window_id()
-		for win_id in Window.get_window_ids():
-			w = Window.from_id(int(win_id))
-			if win_id == active_window_id:
+		for id in Window.get_window_ids():
+			w = Window.from_id(int(id))
+			if id == active_window_id:
 				w.active = True
 			if display.intern_atom("_NET_WM_ACTION_MOVE") in w.allowed_actions or display.intern_atom("_NET_WM_ACTION_RESIZE") in w.allowed_actions:
 				windows.append(w)
@@ -142,7 +138,7 @@ class Window:
 		return Window(display.create_resource_object('window', wid), wid, full)
 
 
-	# <id>	<geometry>	<frame_extents>	<window_workspace>:<current_workspace>	<tags>	<class>	<title>
+	# <id> <geometry> <frame_extents> <window_workspace>:<current_workspace> <tags> <class> <title>
 	def get_info_string(self):
 		tags = ","
 		if self.hidden:
@@ -152,7 +148,7 @@ class Window:
 		if self.active:
 			tags += "active,"
 		current_workspace = Window.get_current_workspace();
-		return "0x%x\t%s\t%s\t%d:%d\t%s\t%s\t%s" % (self.win_id, "%dx%d+%d+%d" % (self.width, self.height, self.x, self.y), "%d,%d,%d,%d" % (self.extents_top, self.extents_bottom, self.extents_left, self.extents_right), self.workspace, current_workspace, tags, self.wm_class, self.name)
+		return "0x%x\t%s\t%s\t%d:%d\t%s\t%s\t%s" % (self.id, "%dx%d+%d+%d" % (self.width, self.height, self.x, self.y), "%d,%d,%d,%d" % (self.extents_top, self.extents_bottom, self.extents_left, self.extents_right), self.workspace, current_workspace, tags, self.wm_class, self.name)
 
 
 	def move(self, x, y, width, height):
@@ -161,22 +157,21 @@ class Window:
 		width -= self.extents_left + self.extents_right
 		height -= self.extents_top + self.extents_bottom
 
-		gravity=0
-		gravity_flags = gravity | 0b0000000010000000 # ???
-		gravity_flags = gravity_flags | 0b0000000100000000 # x
-		gravity_flags = gravity_flags | 0b0000001000000000 # y
-		gravity_flags = gravity_flags | 0b0000010000000000 # w
-		gravity_flags = gravity_flags | 0b0000100000000000 # h
+		flags = 0 | 0b0000000010000000 # ???
+		flags = flags | 0b0000000100000000 # x
+		flags = flags | 0b0000001000000000 # y
+		flags = flags | 0b0000010000000000 # w
+		flags = flags | 0b0000100000000000 # h
 		wm_normal_hints = window.get_wm_normal_hints()
 		if wm_normal_hints != None:
 			wm_normal_hints["width_inc"] = 0
 			wm_normal_hints["height_inc"] = 0
 			window.set_wm_normal_hints(wm_normal_hints)
-		self.set_property('_NET_MOVERESIZE_WINDOW', [gravity_flags, int(x), int(y), int(width), int(height)])
+		self.set_property('_NET_MOVERESIZE_WINDOW', [flags, int(x), int(y), int(width), int(height)])
 
 
 	def get_property(self, prop):
-		atom = self.win.get_full_property(display.intern_atom(prop), X.AnyPropertyType)
+		atom = self.win.get_full_property(display.intern_atom(prop), AnyPropertyType)
 		if atom:
 			return atom.value
 		else:
@@ -190,11 +185,11 @@ class Window:
 			data = (data+[0]*(5-len(data)))[:5]
 			datasize = 32
 
-		event = protocol.event.ClientMessage(window=self.win, client_type=display.intern_atom(prop), data=(datasize, data))
+		e = ClientMessage(window=self.win, client_type=display.intern_atom(prop), data=(datasize, data))
 
 		if not mask:
-			mask = (X.SubstructureRedirectMask|X.SubstructureNotifyMask)
-		display.screen().root.send_event(event, event_mask=mask)
+			mask = (SubstructureRedirectMask | SubstructureNotifyMask)
+		display.screen().root.send_event(e, event_mask=mask)
 
 
 	@staticmethod
@@ -213,4 +208,4 @@ class Window:
 
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	main(argv[1:])
